0d57b7b027ed489f571cd51b8fdff919c4379134
sktollman <stollman@stanford.edu>
2018-05-28 18:54:39 -0700

    Adds comparison to original results (#7)
    Adds `-o` command-line argument to `figure2_plot.py` to plot the original results in comparison to our results.
    Also adds `reverse_engineer_points.py`, which computes the approximate original results from the image in the paper.

--- figure2_plot.py
+++ figure2_plot.py
@@ -11,6 +11,9 @@ from functools import reduce
 
 Stats = namedtuple('Stats', ['util', 'delay'])
 
+PARETO_COLOR = 'red'
+PARETO_COLOR_LIGHT = '#FFAEB9'
+
 COLORS = {
     'abc': 'blue',
     'cubicpie': 'black',
@@ -33,8 +36,30 @@ NAMES = {
     'verus': 'Verus'
 }
 
+FIGURE2A_ORIGINAL = {
+    'abc': (191.95834358517678, 0.8895608259285301),
+    'bbr': (486.8920433391686, 0.9399028319376079),
+    'cubic': (445.5895460340233, 0.9801764367448698),
+    'cubiccodel': (177.88015620661164, 0.6188327047241577),
+    'cubicpie': (153.80228344469558, 0.5282170939078181),
+    'sprout': (174.8117213747123, 0.3984465895288627),
+    'vegas': (341.5402884053896, 0.9113756951991305),
+    'verus': (354.68196232307116, 0.8627117560570223)
+}
+FIGURE2B_ORIGINAL = {
+    'abc': (194.61469364942175, 0.8567136150234742),
+    'bbr': (579.8963509590329, 0.9323004694835679),
+    'cubic': (557.3238874983415, 0.9743661971830986),
+    'cubiccodel': (207.58455812573303, 0.6207511737089202),
+    'cubicpie': (172.76170142715284, 0.5510798122065728),
+    'sprout': (178.69145410543587, 0.38215962441314555),
+    'vegas': (591.5231350767796, 0.9296713615023473),
+    'verus': (419.5871922555574, 0.8218779342723004)
+}
+ORIGINAL_FIGURES = { '2a': FIGURE2A_ORIGINAL, '2b': FIGURE2B_ORIGINAL }
+
 # based off of https://sirinnes.wordpress.com/2013/04/25/pareto-frontier-graphic-via-python/
-def plot_pareto_frontier(Xs, Ys, maxX=True, maxY=True):
+def plot_pareto_frontier(Xs, Ys, color, linestyle, maxX=True, maxY=True):
     '''Pareto frontier selection process'''
     sorted_list = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxY)
     pareto_front = [sorted_list[0]]
@@ -45,15 +70,17 @@ def plot_pareto_frontier(Xs, Ys, maxX=True, maxY=True):
     '''Plotting process'''
     pf_X = [pair[0] for pair in pareto_front]
     pf_Y = [pair[1] for pair in pareto_front]
-    plt.plot(pf_X, pf_Y, color='red', linestyle='dashed', linewidth=1)
+    plt.plot(pf_X, pf_Y, color=color, linestyle=linestyle, linewidth=1)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data-filename', default=None,
+    parser.add_argument('-d', '--data-filename', default=None,
         help='csv file from which to read data', type=str, nargs='+')
-    parser.add_argument('--plot-filename', default=None,
+    parser.add_argument('-p', '--plot-filename', default=None,
         help='svg file to save plot', type=str)
+    parser.add_argument('-o', '--original-figure', default=None,
+        help='original result to plot [2a, 2b]', type=str)
     args = parser.parse_args()
 
     stats = dict()
@@ -63,10 +90,7 @@ if __name__ == '__main__':
         for fn in args.data_filename:
             with open(fn) as f:
                 for l in f:
-                    results = l.split(', ')
-                    proto = results[0]
-                    util = results[1]
-                    delay = results[2]
+                    proto, util, delay, throughput, power = l.split(', ')
                     temp_stats[proto].append(Stats(float(util), float(delay)))
         for proto, stats_list in temp_stats.items():
             avg_util = reduce(lambda x, y: x + y,
@@ -102,17 +126,33 @@ if __name__ == '__main__':
     ax1.xaxis.set_major_formatter(NullFormatter())
     ax1.xaxis.set_minor_formatter(ScalarFormatter())
 
-    Xs = []
-    Ys = []
     for proto, s in stats.items():
-        if proto != 'abc':
-            Xs.append(s.delay)
-            Ys.append(s.util)
-        ax1.plot(s.delay, s.util, 'o', color=COLORS[proto], label=NAMES[proto])
+        ax1.plot(s.delay, s.util, 'o', markersize=7, markeredgewidth=1, markeredgecolor=COLORS[proto],
+            markerfacecolor='None', label=NAMES[proto])
         ax1.annotate(NAMES[proto], xy=(s.delay, s.util),
             xytext=(s.delay + 15, s.util), color=COLORS[proto])
 
-    plot_pareto_frontier(Xs, Ys, maxY=False)
+    plot_pareto_frontier(
+        [s.delay for proto, s in stats.items() if proto != 'abc'],
+        [s.util for proto, s in stats.items() if proto != 'abc'],
+        PARETO_COLOR_LIGHT, ':', maxY=False)
+
+    # line between original and reproduction results
+    if args.original_figure:
+        if args.original_figure not in ORIGINAL_FIGURES:
+            raise Exception('No data found for that figure')
+        orig = ORIGINAL_FIGURES[args.original_figure]
+
+        for proto, point in orig.items():
+            if proto not in stats: continue
+            s = stats[proto]
+            ax1.plot(point[0], point[1], 'o', markersize=7, color=COLORS[proto])
+            ax1.plot([s.delay, point[0]], [s.util, point[1]],'k-', color=COLORS[proto])
+
+        plot_pareto_frontier(
+            [point[0] for proto, point in orig.items() if proto != 'abc'],
+            [point[1] for proto, point in orig.items() if proto != 'abc'],
+            PARETO_COLOR, '--', maxY=False)
 
     # save plot to file
     if args.plot_filename:
