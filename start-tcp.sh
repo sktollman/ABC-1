b5fd5e55557ed1d6ced02d0d12a87e5820059ea4
Alan Flores-Lopez <alanf94@stanford.edu>
2018-06-03 18:27:43 +0000

    cubic processes die well

--- start-tcp.sh
+++ start-tcp.sh
@@ -3,7 +3,4 @@ if [ $1 = "bbr" ] ; then
 	sudo tc qdisc add dev ingress root fq pacing
 	sudo tc qdisc show dev ingress
 fi
-iperf -c $MAHIMAHI_BASE -p 42425 -w 16m -t 140 -Z $1 &
-sleep 140
-killall iperf
-killall iperf
+iperf -c $MAHIMAHI_BASE -p 42425 -w 16m -t 1000 -Z $1
