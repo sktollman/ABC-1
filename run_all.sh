ccd29d8fd426654fe7b6260c8b4591b41ee394f6
Sarah Tollman <stollman@stanford.edu>
2018-06-09 18:01:49 -0700

    Remove loop

--- run_all.sh
+++ run_all.sh
@@ -7,61 +7,56 @@
 
 times_to_run_figs=5
 
-for i in {1..5}
-do
+echo "MASTER ITERATION: 1"
 
-  echo "MASTER ITERATION: $i"
+# 1. Generate results for figure1
+python reproduction/experiment.py \
+  --experiment figure1 \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --csv-out figure1.csv \
 
-  # 1. Generate results for figure1
-  python reproduction/experiment.py \
-    --experiment figure1 \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --csv-out figure1.csv \
+mv figure1.csv results/
 
-  mv figure1.csv results/
+# 2. Generate results for figure2a
+python reproduction/experiment.py \
+  --experiment figure2a \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --num-runs $times_to_run_figs \
 
-  # 2. Generate results for figure2a
-  python reproduction/experiment.py \
-    --experiment figure2a \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --num-runs $times_to_run_figs \
-
-  # 3. Generate results for figure2b
-  python reproduction/experiment.py \
-    --experiment figure2b \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --num-runs $times_to_run_figs \
+# 3. Generate results for figure2b
+python reproduction/experiment.py \
+  --experiment figure2b \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --num-runs $times_to_run_figs \
 
 
-  # 4. Generate results for figure2 bothlinks
-  python reproduction/experiment.py \
-    --experiment bothlinks \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --num-runs $times_to_run_figs \
+# 4. Generate results for figure2 bothlinks
+python reproduction/experiment.py \
+  --experiment bothlinks \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --num-runs $times_to_run_figs \
 
-  # 5. Gather results for 2a, 2b, and bothlinks
+# 5. Gather results for 2a, 2b, and bothlinks
 
-  python reproduction/utils/gather_multiple_results.py \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --experiment figure2a \
-    --num-runs $times_to_run_figs > results/figure2a.csv
+python reproduction/utils/gather_multiple_results.py \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --experiment figure2a \
+  --num-runs $times_to_run_figs > results/figure2a.csv
 
-  python reproduction/utils/gather_multiple_results.py \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --experiment figure2b \
-    --num-runs $times_to_run_figs > results/figure2b.csv
+python reproduction/utils/gather_multiple_results.py \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --experiment figure2b \
+  --num-runs $times_to_run_figs > results/figure2b.csv
 
-  python reproduction/utils/gather_multiple_results.py \
-    --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
-    --experiment bothlinks \
-    --num-runs $times_to_run_figs > results/bothlinks.csv
+python reproduction/utils/gather_multiple_results.py \
+  --schemes abc sprout cubicpie cubiccodel verus vegas cubic bbr copa ledbat pcc \
+  --experiment bothlinks \
+  --num-runs $times_to_run_figs > results/bothlinks.csv
 
 
-  # Move the entire results/logs directory into its own one for this iteration.
-  mv results results-$i
-  mkdir results
+# Move the entire results/logs directory into its own one for this iteration.
+mv results results-1
+mkdir results
 
-  mv logs logs-$i
-  mkdir logs
-
-done
+mv logs logs-1
+mkdir logs
